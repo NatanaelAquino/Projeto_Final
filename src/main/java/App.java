3f9edc1fe213bb41a4d9441f04ab03bebf092f57
Natnael Aquino <wedit62@gmail.com>
2024-06-08 13:28:19 -0300

    Sua mensagem de commit aqui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,16 @@
 package main.java;
 
-import main.java.entities.GUI.TelaLogin;
 import main.java.DAO.DatabaseQuery;
 import main.java.entities.GUI.Criar;
-import main.java.entities.GUI.Descarte;
 import main.java.entities.GUI.DescarteNovo;
-import main.java.entities.GUI.Home;
 import main.java.entities.GUI.Login;
 import main.java.entities.GUI.Menu;
-import main.java.entities.GUI.Perfil;
 import main.java.entities.GUI.PerfilNovo;
-import main.java.entities.GUI.Desempenho;
 import main.java.entities.GUI.DesempenhoNovo;
 import main.java.entities.GUI.Editar;
 
 public class App {
-    public static TelaLogin telaLogin = new TelaLogin();
-    public static Home home = new Home();
-    public static Perfil perfil = new Perfil();
-    public static Descarte descartado = new Descarte();
-    public static Desempenho desempenho = new Desempenho();
+   
     public static String userID;
     public static DatabaseQuery dados = new DatabaseQuery();
     public static Menu menu = new Menu();
